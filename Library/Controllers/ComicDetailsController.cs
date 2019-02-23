5f6c5cd60723ea40445aec44e99205be5d78b6f7
Martin Velez <martindelestero@gmail.com>
2019-02-23 19:30:20 +1300

    viewBag property

--- Library/Controllers/ComicDetailsController.cs
+++ Library/Controllers/ComicDetailsController.cs
@@ -19,6 +19,16 @@ namespace Library.Controllers
             if (DateTime.Today.DayOfWeek == DayOfWeek.Saturday || DateTime.Today.DayOfWeek == DayOfWeek.Sunday)
             {
                 //  return Redirect("/");
+
+                ViewBag.SeriesTitle = "Spider Man";
+                ViewBag.IssueNumber = 700;
+                ViewBag.Description = "Final issue";
+                ViewBag.artists = new string[]  // array to add artists, if statement to check if there are any content to render, if false do not render
+                    {
+                        "Script: Ana bonetto",
+                        //"Edition: Shan kumar"
+                    };
+
                 return View();
             }
 
